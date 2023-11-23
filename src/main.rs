661bd3495509b1ea8f2e00d51bca5e789aa03bbf
ico <104563806+icomev@users.noreply.github.com>
2023-11-23 12:01:15 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -8,76 +8,83 @@ mod server;
 
 
 use crate::exec::gate_exec::run_gate;
-//use crate::exec::bybit_exec::run_bybit;
 use crate::exec::kucoin_exec::run_kucoin;
 use crate::exec::server_exec::run_server;
+use crate::state::kucoin_state::KucoinState;
+use crate::state::gate_state::GateState;
 
+
+use reqwest::Client;
+use std::sync::Arc;
 
 
 #[tokio::main]
 async fn main() {
-    let (_, kucoin_result, _) = tokio::join!(
-        run_gate(),
-        run_kucoin(),
-        run_server(), // Assuming run_server is also an async function
-        // You can continue to add more calls here for other exchanges or tasks
-    );
 
-    // Handle the result of run_kucoin
-    if let Err(e) = kucoin_result {
-        eprintln!("Error running kucoin: {:?}", e);
-    } 
+    let kucoin_lesser = Arc::new(Client::new());
+    let kucoin_client = Arc::new(Client::new());
+    let kucoin_state = Arc::new(KucoinState::new());
 
-    // Add any additional cleanup or finalization code here if necessary
+    let gate_lesser = Arc::new(Client::new());
+    let gate_client = Arc::new(Client::new());
+    let gate_state = Arc::new(GateState::new());
+
+
+    let gate_state_clone = Arc::clone(&gate_state);
+    tokio::spawn(async move {
+        run_gate(&gate_lesser, gate_state_clone).await;
+        // Since run_gate returns (), there is no error handling needed here
+    });
+
+    let kucoin_state_clone = Arc::clone(&kucoin_state);
+    tokio::spawn(async move {
+        if let Err(e) = run_kucoin(&kucoin_lesser, kucoin_state_clone).await {
+            eprintln!("Error running kucoin: {:?}", e);
+        }
+    });
+
+    let new_gate_state = Arc::clone(&gate_state);
+    let new_kucoin_state = Arc::clone(&kucoin_state);
+    tokio::spawn(async move {
+        run_server(&gate_client, &kucoin_client, new_gate_state, new_kucoin_state).await;
+        // Since run_server returns (), there is no error handling needed here
+    });
+
+
+    futures::future::pending::<()>().await;
 }
 
+
 /*
-#[tokio::main]
-async fn main() {
-    let client = Arc::new(Client::new());
-    let shared_state = Arc::new(SharedState::new());
 
-    match get_futures_contracts(client.clone()).await {
-        Ok(usdt_contracts) => {
-            shared_state.update_contracts(usdt_contracts);
+    let gate_test = Arc::clone(&gate_state);
+    let test_task = tokio::spawn(async move {
+       tokio::time::sleep(tokio::time::Duration::from_secs(60)).await;
+       // Uncomment if you need to use these lines
+       //bybit_state.print_bybit_contract("DOGEUSDT").await;
+       //tokio::time::sleep(tokio::time::Duration::from_secs(30)).await;
 
-            let ws_url = Url::parse("wss://fx-ws.gateio.ws/v4/ws/usdt").expect("Failed to parse WebSocket URL");
-            let shared_state_for_websocket = shared_state.clone();
+       // Assuming "HIFI" is the value you want to test with
+       let test_value = "MBL".to_string(); // Use the exact key as stored in the state
+       // You would need to ensure `client` and `bybit_state` are available in this scope
+       gate_calc(&test, &gate_test, test_value).await;
+    });
 
-            // Spawn a new task for the WebSocket connection
-            tokio::spawn(async move {
-                loop {
-                    match connect_gate_websocket(&ws_url, shared_state_for_websocket.clone()).await {
-                        Ok(()) => {
-                            let now = Utc::now();
-                            eprintln!("{} - WebSocket connection closed cleanly, will attempt to reconnect...", now.to_rfc3339());
-                        },
-                        Err(e) => {
-                            let now = Utc::now();
-                            eprintln!("{} - Error with WebSocket connection: {:?}", now.to_rfc3339(), e);
-                        }
-                    }
-            
-                    // Reconnection backoff
-                    eprintln!("{} - Attempting to reconnect to WebSocket...", Utc::now().to_rfc3339());
-                    tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
-                }
-            });
 
-            loop {
-                tokio::time::sleep(tokio::time::Duration::from_secs(3600)).await; // Sleep for 1 hour
-                let now = Utc::now();
-                println!("time {}...", now);
-                shared_state.print_specific_contract("DOGE_USDT").await;           
-           
-             }
-        },
-        Err(e) => {
-            eprintln!("Failed to get futures contracts: {:?}", e);
-        },
-    }
+    let kucoin_test = Arc::clone(&kucoin_state);
+    let test_task2 = tokio::spawn(async move {
+       tokio::time::sleep(tokio::time::Duration::from_secs(60)).await;
+       // Uncomment if you need to use these lines
+       //bybit_state.print_bybit_contract("DOGEUSDT").await;
+       //tokio::time::sleep(tokio::time::Duration::from_secs(30)).await;
+
+       // Assuming "HIFI" is the value you want to test with
+       let test_value = "MBL".to_string(); // Use the exact key as stored in the state
+       // You would need to ensure `client` and `bybit_state` are available in this scope
+       kucoin_calc(&test2, &kucoin_test, test_value).await;
+    });
 
 
 
-    
-} */
+   test_task.await.expect("Test task failed");
+   test_task2.await.expect("Test task failed");*/
